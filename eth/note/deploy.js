b6bfb328374fc9de73059a42a844403e4691a3c4
billy <sangyounw@naver.com>
2018-11-22 14:31:51 +0900

    install web with window environment

---
+++ eth/note/deploy.js
@@ -0,0 +1,8 @@
+// deploy
+
+// web3 : ethereum interact
+// git config --global url."https://".insteadOf git://
+// $ npm install --global --production windows-build-tools
+// $ npm install web3
+
+// HDWalletProvider : 12words -> private key 생성
